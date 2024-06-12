db0fcb751afc675438b8a5af1cda2e6b0183fdec
Kim-Taein <103629213+Kim-Taein@users.noreply.github.com>
2024-06-12 15:01:58 +0900

    inference.py

--- inference.py
+++ inference.py
@@ -14,6 +14,7 @@ import argparse
 import os
 import pprint
 
+import json
 import cv2
 import torch
 import torch.nn.parallel
@@ -28,7 +29,7 @@ import matplotlib.pyplot as plt
 import matplotlib.lines as mlines
 import matplotlib.patches as mpatches
 
-import _init_paths
+# import _init_paths
 from config import cfg
 from config import update_config
 from core.loss import JointsMSELoss
@@ -40,8 +41,8 @@ import cython
 import dataset
 import models
 from models.omnipose import OmniPose, get_omnipose
-from models.omnipose import get_Canny_HRNet
-from models.frankenstein import get_frankenstein
+# from models.omnipose import get_Canny_HRNet
+# from models.frankenstein import get_frankenstein
 from core.inference import get_final_preds_no_transform
 
 
@@ -97,14 +98,23 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Visualize MPII predictions')
     parser.add_argument('--dataset', type=str, default='MPII')
     parser.add_argument('--image-path', help='Path of MPII val images',
-                        type=str, default='/home/bm3768/Desktop/research/dataset/COCO/val2017/')
+                        type=str, default='/content/drive/MyDrive/mpii/images')
     parser.add_argument('--gt-anno', help='Path of MPII val annotation', type=str,
-                        default='/home/bm3768/Desktop/research/dataset/COCO/person_keypoints_val2017.json')
+                        default='/content/drive/MyDrive/mpii/annot/valid.json')
     parser.add_argument('--save-path',help="Path to save the visualizations", type=str, default='samples/')
     parser.add_argument('--prediction', help="Prediction file to visualize", type=str, required=True)
     args = parser.parse_args()
 
     return args
+
+
+def transform_coords(coords, original_size, model_input_size=(404, 308)):
+    orig_height, orig_width = original_size
+    model_height, model_width = model_input_size
+    scale_x = orig_width / model_width
+    scale_y = orig_height / model_height
+    transformed_coords = [(int(x * scale_x), int(y * scale_y)) for x, y in coords]
+    return transformed_coords
 
 
 def map_joint_dict(joints):
@@ -117,7 +127,7 @@ def map_joint_dict(joints):
         
     return joints_dict
 
-def plot_COCO_image(preds, img_path, save_path, link_pairs, ring_color, color_ids, save=True):
+def plot_MPII_image(preds, img_path, save_path, link_pairs, ring_color, color_ids, save=True):
     # Read Images
     data_numpy = cv2.imread(img_path, cv2.IMREAD_COLOR | cv2.IMREAD_IGNORE_ORIENTATION)
     data_numpy = cv2.resize(data_numpy, (384,288), interpolation = cv2.INTER_AREA)
@@ -132,26 +142,27 @@ def plot_COCO_image(preds, img_path, save_path, link_pairs, ring_color, color_id
     joints_dict = map_joint_dict(preds[0])
     
     # stick 
-    for k, link_pair in enumerate(link_pairs):
-        lw = 2
-        line = mlines.Line2D(
-                np.array([joints_dict[link_pair[0]][0],
-                          joints_dict[link_pair[1]][0]]),
-                np.array([joints_dict[link_pair[0]][1],
-                          joints_dict[link_pair[1]][1]]),
-                ls='-', lw=lw, alpha=1, color=color_ids[0],)
-        line.set_zorder(0)
-        ax.add_line(line)
+    # for k, link_pair in enumerate(link_pairs):
+    #     if link_pair[0] in joints_dict and link_pair[1] in joints_dict:
+    #         lw = 2
+    #         line = mlines.Line2D(
+    #                 np.array([joints_dict[link_pair[0]][0],
+    #                           joints_dict[link_pair[1]][0]]),
+    #                 np.array([joints_dict[link_pair[0]][1],
+    #                           joints_dict[link_pair[1]][1]]),
+    #                 ls='-', lw=lw, alpha=1, color=color_ids[0],)
+    #         line.set_zorder(0)
+    #         ax.add_line(line)
+            
     # black ring
     for k in range(preds.shape[1]):
         if preds[0,k,0] > w or preds[0,k,1] > h:
             continue
         radius = 2
-
         circle = mpatches.Circle(tuple(preds[0,k,:2]), 
                                  radius=radius, 
                                  ec='black', 
-                                 fc=ring_color[k], 
+                                 fc=ring_color[k % len(ring_color)], 
                                  alpha=1, 
                                  linewidth=1)
         circle.set_zorder(1)
@@ -163,24 +174,46 @@ def plot_COCO_image(preds, img_path, save_path, link_pairs, ring_color, color_id
     plt.subplots_adjust(top=1,bottom=0,left=0,right=1,hspace=0,wspace=0)        
     plt.margins(0,0)
     print(save_path)
-    plt.savefig(save_path, format='jpg', bbox_inches='tight', dpi=100)
+    plt.savefig(save_path, format='jpg', bbox_inches='tight', pad_inches=0)
     plt.close()
+
+
+def save_all_keypoints_to_json(all_preds, save_path):
+    with open(save_path, 'w') as f:
+        json.dump(all_preds, f, indent=4)
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train keypoints network')
     # general
     parser.add_argument('--cfg', help='experiment configure file name',
-                        default='experiments/coco/hrnet/w48_384x288_adam_lr1e-3.yaml', type=str)
+                        default='/content/drive/MyDrive/OmniPose/experiments/mpii/omnipose_w48_256x256yaml.yaml', type=str)
     parser.add_argument('--opts', help="Modify config options using the command-line",
                         default=None, nargs=argparse.REMAINDER)
     parser.add_argument('--modelDir', help='model directory', type=str, default='')
     parser.add_argument('--logDir', help='log directory', type=str, default='')
     parser.add_argument('--dataDir', help='data directory', type=str, default='')
     parser.add_argument('--prevModelDir', help='prev Model directory', type=str, default='')
+    parser.add_argument('--model-file', help='path to the pretrained model file', required=True, type=str)
 
     args = parser.parse_args()
     return args
+
+
+def calculate_bbox_info(joints):
+    visible_joints = [joint for joint in joints if joint[0] > 0 and joint[1] > 0]
+    if not visible_joints:
+        return (0, [0, 0])
+
+    min_x = min(joint[0] for joint in visible_joints)
+    max_x = max(joint[0] for joint in visible_joints)
+    min_y = min(joint[1] for joint in visible_joints)
+    max_y = max(joint[1] for joint in visible_joints)
+
+    center = [(min_x + max_x) / 2, (min_y + max_y) / 2]
+    scale = max(max_x - min_x, max_y - min_y) / 200.0
+
+    return (scale, center)
 
 
 def main(args):
@@ -200,7 +233,10 @@ def main(args):
 
     model = eval('models.omnipose.get_omnipose')(cfg, is_train=False)
 
-    if cfg.TEST.MODEL_FILE:
+    if args.model_file:
+        cfg.defrost()
+        cfg.TEST.MODEL_FILE = args.model_file
+        cfg.freeze()
         logger.info("=> loading checkpoint '{}'".format(cfg.TEST.MODEL_FILE))
         checkpoint = torch.load(cfg.TEST.MODEL_FILE)
         best_perf = checkpoint['perf']
@@ -243,8 +279,10 @@ def main(args):
 
     model.eval()
 
-    files_loc = '/home/bm3768/Desktop/research/dataset/NSL/9303/195KB_1'
+    files_loc = '/content/drive/MyDrive/infer_test'
     images = os.listdir(files_loc)
+
+    all_data = []
 
     for idx in range(len(images)):
         print(idx,"/",len(images))
@@ -265,9 +303,33 @@ def main(args):
 
         preds, maxvals = get_final_preds_no_transform(cfg, outputs.detach().cpu().numpy())
 
+        preds = 4*preds
+
+        joint_vis = [1 if maxval[0] > 0.5 else 0 for maxval in maxvals[0]]
+        joints = preds[0].tolist()
+        scale, center = calculate_bbox_info(joints)
+
+        original_image = cv2.imread(img_path)
+        original_size = original_image.shape[:2]  # height, width
+        transformed_keypoints = transform_coords(joints, original_size)
+
+
+        img_data = {
+            "joint_vis": joint_vis,
+            "joints": joints,
+            "image": images[idx],
+            "scale": scale,
+            "center": center
+        }
+
+        all_data.append(img_data)
+
         colorstyle = artacho_style
 
-        plot_COCO_image(4*preds, img_path, 'samples/NSL/9303/195KB_1/'+images[idx], colorstyle.link_pairs, colorstyle.ring_color, colorstyle.color_ids, save=True)
+        plot_MPII_image(preds, img_path, '/content/drive/MyDrive/infer_samples/' + images[idx], colorstyle.link_pairs, colorstyle.ring_color, colorstyle.color_ids, save=True)
+        
+    with open('/content/drive/MyDrive/infer_samples/all_keypoints.json', 'w') as f:
+        json.dump(all_data, f, indent=4)
 
 if __name__ == '__main__':
     arg = parse_args()
